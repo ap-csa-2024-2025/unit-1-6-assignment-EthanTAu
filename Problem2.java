7931141f26b682b7043b09d41b0ebcd4255aeffd
EthanTAu <etau100@student.hbuhsd.edu>
2024-09-10 11:53:30 -0700

    Update Problem2.java

--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,15 @@
+public class problem2
+  {
+    public static void main(String[] args);
+    {
+      double first = 29.5839;
+      double second = 45,4920;
+      int firstr = int (first + 0.5);
+      int secondr = int (second + 0.5);
+      int ans = firstr + secondr;
 
+      System.out.println("Your first number: " + first);
+      System.out.println('Your second number: " + second);
+      System.out.println()
+    }
+  }
