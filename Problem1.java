5ca52f53b18024976c5ecdcf769f64504c04e2d0
EthanTAu <etau100@student.hbuhsd.edu>
2024-09-10 11:45:33 -0700

    home

--- Problem1.java
+++ Problem1.java
@@ -1,7 +1,13 @@
-public class Problem1
+public class problem1
 {
   public static void main(String[] args)
   {
-    // insert code here
+    int numerator = 8;
+    int denominator = 3;
+    double answer = double numerator / denominator;
+
+    System.out.println("Your numerator is: " + numerator);
+    System.out.println("Your denominator is: " + denominator);
+    System.out.println("Your answer in decimal form is: " + answer);
   }
 }
